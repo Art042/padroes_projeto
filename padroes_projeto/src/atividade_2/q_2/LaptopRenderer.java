0d9e63aac94549d494be29e62ac3e3a19c992ea9
Art042 <artur.tourinho@acad.ifma.edu.br>
2023-03-21 00:29:04 -0300

    atv02 questao 02

---
+++ padroes_projeto/src/atividade_2/q_2/LaptopRenderer.java
@@ -0,0 +1,7 @@
+package atividade_2.q_2;
+
+public class LaptopRenderer implements ImageManagerStrategy{
+	public void mostrarImagem() { 
+		System.out.println("Mostrando uma imagem no laptop..." ); 
+		} 
+}
---
+++ padroes_projeto/src/atividade_2/q_2/SmartphoneRenderer.java
@@ -0,0 +1,8 @@
+package atividade_2.q_2;
+
+public class SmartphoneRenderer implements ImageManagerStrategy{
+	public void mostrarImagem () { 
+		System.out.println ( "Mostrando uma imagem no smartphone..." ); 
+		} 
+
+}
